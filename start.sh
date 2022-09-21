efd288ba22d7fe82893b75a5a2e7c2b1a1db37d3
Ruan Andre Felssner <ruan_felssner@hotmail.com>
2022-09-21 16:54:18 -0300

    Add resources

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 git pull
-bash /home/ubuntu/realrp/server/run.sh +exec /home/ubuntu/realrp/server-data/server.cfg
+bash /home/ubuntu/realrp/server/run.sh +exec /home/ubuntu/realrp/resources/server.cfg
