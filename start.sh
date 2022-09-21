bd2b00d34394e5de560c633056b30a06094ef2a2
Ruan Andre Felssner <ruan_felssner@hotmail.com>
2022-09-21 18:01:52 -0300

    Start

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 git pull
-bash /home/ubuntu/realrp/server/run.sh +exec /home/ubuntu/realrp/resources/server.cfg
+bash /home/ubuntu/realrp/server/run.sh +exec /home/ubuntu/realrp/resources/server.cfg +set sv_enforceGameBuild 2189
